fa5421f5a817298b317258bef54b90b220e66bdc
AppleD0g <user@Users-MacBook-Pro.local>
2025-04-10 23:43:40 +0700

    add fonts

--- lib/presentation/screens/auth/login_screen.dart
+++ lib/presentation/screens/auth/login_screen.dart
@@ -1,20 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../../../app/controllers/auth_controller.dart';
+import '../../../app/themes/colors.dart';
 
 class LoginScreen extends StatelessWidget {
+
+  const LoginScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    final AuthController authController =
-        Get.find(); // Find existing controller
+    final AuthController authController = Get.find(); // Find existing controller
 
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: authController.login, // Call login method
-          child: Text("Login"),
-        ),
-      ),
-    );
+        backgroundColor: AppColors.background,
+        body: Column(
+          children: [
+            const Text("Login Screen", style: TextStyle(color: Colors.white)),
+            const SizedBox(height: 20),
+            const Text("This is a new testing", style: TextStyle(color: Colors.white, fontFamily: "Poppins", fontSize: 20)),
+          const Text("This is a new testing", style: TextStyle(color: Colors.white, fontSize: 20)),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: authController.login, // Call login method
+              child: const Text("This is a new testing"),
+            ),
+          ]
+        ,
+        ),);
   }
 }
