3b45044e44807ba9df91de219c562d5ed91e1a88
AppleD0g <user@Users-MacBook-Pro.local>
2025-04-11 02:07:58 +0700

    fixing typo

--- lib/app/themes/font_size.dart
+++ lib/app/themes/font_size.dart
@@ -1,8 +1,8 @@
 class FontSize {
   static const double xs = 12;
-  static const double s = 14;
-  static const double m = 16;
-  static const double l = 18;
+  static const double sm= 14;
+  static const double md = 16;
+  static const double lg = 18;
   static const double xl = 22;
   static const double xxl = 26;
   static const double xxxl = 32;
