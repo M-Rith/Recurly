fa1f5959d8b43be62a502a090014cc71a802c59f
AppleD0g <user@Users-MacBook-Pro.local>
2025-04-11 21:16:50 +0700

    add auth screen

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -1,4 +1,6 @@
 class AppRoutes {
   static const String HOME = '/home';
   static const String LOGIN = "/login";
+  static const String SIGN_UP = "/sign_up";
+  static const String AUTH = "/auth";
 }
